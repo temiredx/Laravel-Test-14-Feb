36d3957c2e5332b8675625cc7e38f28e8c4657c5
Tim <temiredx@gmail.com>
2025-02-14 13:38:34 +0200

    basic architecture

--- database/migrations/2025_02_14_112248_create_tasks_table.php
+++ database/migrations/2025_02_14_112248_create_tasks_table.php
@@ -10,11 +10,12 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('status')->default(false);
+            $table->timestamp('finished')->nullable();
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
